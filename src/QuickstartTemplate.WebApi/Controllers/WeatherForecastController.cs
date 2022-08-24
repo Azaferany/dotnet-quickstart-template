1e5ceaebcd8fe3b4a387a6823e62a7bad482d691
ali zaferany <ali.zaferany79@gmail.com>
2022-08-24 16:57:52 +0430

    debug

--- src/QuickstartTemplate.WebApi/Controllers/WeatherForecastController.cs
+++ src/QuickstartTemplate.WebApi/Controllers/WeatherForecastController.cs
@@ -24,7 +24,21 @@ public class WeatherForecastController : ControllerBase
     {
         _logger = logger;
     }
-
+    /// <summary>
+    /// Get Weather Forecast
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet(Name = "GetWeatherForecast")]
+    public ActionResult<List<WeatherForecast>> Get2()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                         {
+                             Date = DateTime.Now.AddDays(index),
+                             TemperatureC = Random.Shared.Next(-20, 55),
+                             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                         })
+                         .ToList();
+    }
     /// <summary>
     /// Get Weather Forecast
     /// </summary>
